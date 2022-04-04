a409373663e13b4399b24ee0f0c7f57283e01085
BrouGabri3l <gabriel-rodriguesbs@hotmail.com>
2022-04-04 10:31:42 -0300

    Atualizando Db Model

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,4 +1,4 @@
 //Definindo modelo do bancod de dados
 const mongoose = require('mongoose')
-const User = mongoose.model('User', { nome: String, nascimento: Date, email: String, sexo: String, endereco: String, _endNumero: Number, _endBairro: String, CEP: String, UF: String, Cidade: String })
+const User = mongoose.model('User', { nome: String, nascimento: Date, email: String, sexo: String, endereco: String, _endNumero: Number, _endBairro: String, CEP: String, UF: String, cidade: String })
 module.exports = User
